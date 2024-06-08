ec3554c13ca4acdd1bb0ec165ed62d518a9c6c78
PopeLeoXIII <superegd99@gmail.com>
2024-06-08 13:56:41 +0400

    конфигурация бд из файла

--- src/main/java/org/example/db/ConnectionManagerImpl.java
+++ src/main/java/org/example/db/ConnectionManagerImpl.java
@@ -7,20 +7,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ConnectionManagerImpl implements ConnectionManager {
-    private static HikariConfig config = new HikariConfig();
-    private static HikariDataSource ds;
+    private static final HikariConfig config = new HikariConfig("/db.properties");
+    private static final HikariDataSource ds  = new HikariDataSource(config);
     private static ConnectionManager instance;
-
-    static {
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/vehicle");
-        config.setUsername("root");
-        config.setPassword("123");
-//        config.addDataSourceProperty("cachePrepStmts", "true");
-//        config.addDataSourceProperty("prepStmtCacheSize", "250");
-//        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        ds = new HikariDataSource(config);
-    }
 
     public static ConnectionManager getInstance() {
         if (instance == null) {
@@ -28,7 +17,6 @@ public class ConnectionManagerImpl implements ConnectionManager {
         }
         return instance;
     }
-
 
     @Override
     public Connection getConnection() throws SQLException {
