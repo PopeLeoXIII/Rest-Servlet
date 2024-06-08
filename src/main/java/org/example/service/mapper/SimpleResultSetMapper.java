d2515606d541ab3f060da801a0c2bc49bf84b289
PopeLeoXIII <superegd99@gmail.com>
2024-06-08 14:04:31 +0400

    удалены классы шаблона

--- src/main/java/org/example/service/mapper/SimpleResultSetMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.service.mapper;
-
-import org.example.model.SimpleEntity;
-
-import java.sql.ResultSet;
-
-public interface SimpleResultSetMapper {
-    SimpleEntity map(ResultSet resultSet);
-}
